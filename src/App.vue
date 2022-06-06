95c6956b279545e9ab312557ee0871971dda82b1
miloudeveen <miloudeveen@gmail.com>
2022-06-06 15:58:27 +0200

    re-init project with vite and tailwind

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Introduction from "./components/Introduction.vue";
+
+</script>
+
+<template>
+      <Introduction class="text-3xl font-bold" name="Milou de Veen"/>
+     <Introduction class="text-1l font-light" jobFunction="Front-end Developer"/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Introduction.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="introduction">
+    <h1>{{ name }}</h1>
+    <h2>{{ jobFunction }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Introduction",
+  props: {
+    name: String,
+    jobFunction: String,
+    test: Number
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
